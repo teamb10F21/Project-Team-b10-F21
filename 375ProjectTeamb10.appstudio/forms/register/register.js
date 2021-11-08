28612a6ebc5c07bb6d73af2a24f6b6ba54cdbee0
Michael-Petrakis <90578378+Michael-Petrakis@users.noreply.github.com>
2021-11-08 15:41:50 -0600

    Register Form

---
+++ 375ProjectTeamb10.appstudio/forms/register/register.js
@@ -0,0 +1,25 @@
+// This is your blank starting team project - Dr. C
+let req = ""
+let query = ""
+let results = ""
+let netID = "map40414"
+let pw = "Password"
+let databaseSchema = "375groupb10"
+
+
+btnRegister.onclick=function(){
+  let registerUsername = inptRegisterUsername.value
+  let registerPassword = inptRegisterPassword.value
+  let registerName = inptRegisterName.value
+  let registerStreet = inptRegisterStreet.value
+  let registerCity = inptRegisterCity.value
+  let registerState = inptRegisterState.value
+  query = "INSERT INTO customer (username,password,name) VALUES ('" + registerUsername + "', '" + registerPassword + "', '" + registerName + "')"
+  req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=" + netID + "&pass=" + pw + "&database=" + databaseSchema + "&query=" + query)
+  
+  
+  /*query = 'SELECT * FROM customer'
+  results = JSON.parse(req.responseText)
+  lblMessage.value = results
+  */
+}
