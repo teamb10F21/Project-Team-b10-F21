77977c5ed121b7e33c55686a1c7a04d756260ff6
Madison Schirmer <90575585+m-schirmer@users.noreply.github.com>
2021-11-10 16:12:54 -0600

    test

---
+++ 375ProjectTeamb10.appstudio/forms/diets/diets.js
@@ -0,0 +1,25 @@
+btnAtkinsDiet.onclick=function(){
+  ChangeForm(atkinsDiet)
+}
+
+btnLowCarbDiet.onclick=function(){
+  ChangeForm(lowCarbDiet)
+}
+
+btnNoSugarDiet.onclick=function(){
+  ChangeForm(noSugarDiet)
+}
+
+btnPaleoDiet.onclick=function(){
+  ChangeForm(paleoDiet)
+}
+
+btnKetoDiet.onclick=function(){
+  ChangeForm(ketoDiet)
+}
+
+
+btnPescetarianDiet.onclick=function(){
+  ChangeForm(pescetarianDiet)
+}
+
